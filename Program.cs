581513d45e094c41314aef655e2cc44362e11472
Jakub <kubowski2000@gmail.com>
2021-12-24 22:40:46 +0100

    POSK3
    Skonczone

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using POSK3.Ekrany;
+using System;
 using System.Windows.Forms;
 
 namespace POSK3
@@ -16,7 +14,7 @@ namespace POSK3
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new LogowanieForm());
         }
     }
 }
